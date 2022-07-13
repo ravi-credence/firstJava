7daea88fc0dfe765c54dd7cca5199c22ed9f174b
ravi-credence <109064868+ravi-credence@users.noreply.github.com>
2022-07-13 14:39:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World, How Are You ? My Dear Friend... Yo Fine ? ");
+    System.out.println("Hello, World, How Are You ? ");
   }
 }
